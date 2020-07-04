c7c56d97a6ec7384914de555e8c3459d1c86f1cf
1 <7@7r7r.ru>
2020-07-05 03:32:30 +0500

    12

---
+++ www/js/date.js
@@ -0,0 +1,35 @@
+var a = {
+    formData        :new FormData(),//форма отправки
+    a               :       0,// таймер ajax
+    t               :       30,// установка таймера ajax
+    he              :       1,
+    src             :       0,
+    fileName        :       'Demo727.txt',
+    fileText        :       'Привет',
+    fileResult      :       '',
+    url             :       'http://7r7r.ru/android/json.php',
+    post            :       [],
+    m               :       {
+        power   : {
+            title: 'Отчет об Энергии',
+            background: '#bbecff',
+                    url: `http://7r7r.ru/android/sv.php`
+        }
+    },
+    mp3_files        :       {
+        'err.mp3'     :   {},
+        '1.mp3'       :   {},
+        '2.mp3'       :   {},
+        '3.mp3'       :   {},
+        '4.mp3'       :   {},
+        '5.mp3'       :   {},
+        '6.mp3'       :   {},
+        '7.mp3'       :   {},
+        'off.mp3'     :   {},
+        'on.mp3'      :   {},
+        'sv.mp3'      :   {},
+        'ud.mp3'      :   {}
+    }
+
+
+};
